3142a68b8e0278113163deb4ecb88ea497063a0b
Aleksei Severianov <aseverianov@evolution.com>
2023-04-17 20:50:09 +0100

    Robot improvements

--- src/robot/RoborModel.tsx
+++ src/robot/RoborModel.tsx
@@ -1,35 +1,47 @@
 import "@babylonjs/loaders/glTF";
 import { observer } from "mobx-react";
-import React, { Suspense, useState } from "react";
+import React, { Suspense, useCallback, useRef } from "react";
 import { ILoadedModel, Model } from "react-babylonjs";
 import { useStores } from "../hooks/useStores";
+import { Animation } from "../domain/animation";
 
 export const RobotModel: React.FC = observer(function Room() {
   const { animationStore } = useStores();
   const { animation } = animationStore;
-  const [model, setModel] = useState<ILoadedModel | null>(null);
-  const [currentAnimation, setCurrentAnimation] = useState(animation);
+  // const [model, setModel] = useState<ILoadedModel | null>(null);
+  const modelRef = useRef<ILoadedModel | null>(null);
+  const animationRef = useRef<Animation>(animation);
+  // const [currentAnimation, setCurrentAnimation] = useState(animation);
 
-  const onModelLoaded = (m: ILoadedModel) => {
-    setModel(m);
-  };
+  const onModelLoaded = useCallback((m: ILoadedModel) => {
+    // setModel(m);
+    modelRef.current = m;
+    m.animationGroups![animationRef.current].start(true);
+  }, [modelRef, animationRef]);
+
+  console.log("ROBOT render", animation, animationRef.current);
 
   React.useEffect(() => {
-    if (model) {
+    console.log("ROBOT effect", animation, animationRef.current);
+    const model = modelRef.current;
+    const currentAnimation = animationRef.current;
+    if (model && currentAnimation !== animation) {
       model.animationGroups![currentAnimation].stop();
       model.animationGroups![currentAnimation].reset();
       model.animationGroups![animation].start(true);
-      setCurrentAnimation(animation);
+      console.log("ROBOT setCA", animation);
+      // setCurrentAnimation(animation);
+      animationRef.current = animation;
     }
-  }, [animation, currentAnimation, model]);
+  }, [animation, animationRef, modelRef]);
 
   return (
-    <Suspense fallback={"{Loading..."}>
+    <Suspense fallback={"Loading..."}>
       <Model
         name="robot-model"
         rootUrl={require(".//asserts/RobotExpressive.glb")}
         sceneFilename=""
-        onModelLoaded={(e) => onModelLoaded(e)}
+        onModelLoaded={onModelLoaded}
       />
     </Suspense>
   );
